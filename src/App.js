726c258a86256c3ca4166323df23d03fd187012b
ManuelNelson7 <manuel.nelson7@hotmail.com>
2022-03-22 20:57:43 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h3>Éste es el wireframe hecho en figma para el landing de mi ecommerce:</h3>
+      <img src="/img/screenshot.png" alt="" />
+    </>
+  );
+}
+
+export default App;
